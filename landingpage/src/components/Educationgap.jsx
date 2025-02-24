6d539deca273b4b70d9a47f962d62cdc8954b624
James Ogbonna <ogbonnasunday42@gmail.com>
2025-02-24 07:36:29 +0100

    added numbering to the carousel

--- landingpage/src/components/Educationgap.jsx
+++ landingpage/src/components/Educationgap.jsx
@@ -23,7 +23,7 @@ const Educationgap = () => {
                     <div className='p-4'>
                         <div className='grid grid-cols-12'>
                             <div className='col-span-12 lg:col-span-6 md:p-4 p-0 mb-5'>
-                                <h1 className='font-semibold'>The Problem:</h1>
+                                <h1 className='font-semibold'>1. The Problem:</h1>
                                 <h1 className='font-semibold text-2xl'>The Gap Between Education and Employment is Wider Than Ever.</h1>
                                 <p className='text-xl my-5 text-black-50'>67% of employers say graduates lack the skills needed for entry-level roles.</p>
                                 <p className='text-xl'>83% of professionals feel their education didn't prepare them for their careers.</p>
@@ -36,7 +36,7 @@ const Educationgap = () => {
                     <div className='p-4'>
                         <div className='grid grid-cols-12 '>
                             <div className='col-span-12 lg:col-span-6 md:p-4 p-0  mb-5'>
-                                <h1>------This is a hug problem------</h1>
+                                <h1>2. ------This is a hug problem------</h1>
                                 <h1 className='font-semibold text-2xl'>Employment Readiness:</h1>
                                 <p className='text-xl my-5 text-black-50'>65% of children in primary school today will work in a new job type - World Economic Forum; </p>
                                 <p className='text-xl'>Underemployment, Employer Expectations, Workforce Evolution, High Cost of Education-what we bring solution to in career-nexus</p>
@@ -49,7 +49,7 @@ const Educationgap = () => {
                     <div className='p-4'>
                         <div className='grid grid-cols-12'>
                             <div className='col-span-12 lg:col-span-6 md:p-4 p-0  mb-5'>
-                                <h1 className='font-semibold'></h1>-------Guess what we found-----
+                                <h1 className='font-semibold'></h1>3. -------Guess what we found-----
                                 <h1 className='font-semibold text-9xl'> ?</h1>
                             </div>
                             <div className='col-span-12 lg:col-span-6 md:p-4 p-0  mb-5'>
@@ -60,7 +60,7 @@ const Educationgap = () => {
                     <div className='p-4'>
                         <div className='grid grid-cols-12'>
                             <div className='col-span-12 lg:col-span-6 md:p-4 p-0  mb-5'>
-                                <h1 className='font-semibold'>----The solution:</h1>
+                                <h1 className='font-semibold'>4. ----The solution:</h1>
                                 <h1 className='font-semibold text-2xl'>CAREER-NEXUS.COM</h1>
                                 <p className='text-xl my-5 text-black-50'>Revolutionizing the world of skill acquisition and career development;</p>
                                 <p className='text-xl'>An indispensable solution platform for career success!</p>
@@ -73,7 +73,7 @@ const Educationgap = () => {
                     <div className='p-4'>
                         <div className='grid grid-cols-12'>
                             <div className='col-span-12 lg:col-span-6 md:p-4 p-0  mb-5'>
-                                <h1 className='font-semibold'>----At the heat of career-nexus.com---</h1>
+                                <h1 className='font-semibold'>5. ----At the heat of career-nexus.com---</h1>
                                 <h1 className='font-semibold text-2xl'>The Gap Between Education and Employment is Wider Than Ever.</h1>
                                 <p className='text-xl my-5 text-black-50'>Discover your potential with a cutting-edge platform.</p>
                                 <p className='text-xl'>Designed to empower dynamic millennials and generation Z!!</p>
